1a2aa098ef958f6c69d52a45d1e8ea79711e13fa
李景阳 <3306283189@qq.com>
2023-09-15 15:52:12 +0800

    配置封装axios

--- src/service/http.js
+++ src/service/http.js
@@ -8,7 +8,6 @@ const http = {
     /*
     	@params url 代表的是请求地址
     	@params params 代表传递的请求参数
-
      */
     get(url, params) {
         const config = {
@@ -18,7 +17,31 @@ const http = {
         // 如果请求接口的时候传递了params那么我们就把params放到config里面，否在就不把params放在config里面了
         if (params) config.params = params
         return request(config)
+    },
+    post(url, params) {
+        const config = {
+            method: 'post', // 请求方式
+            url // 请求地址
+        }
+        // 如果请求接口的时候传递了params那么我们就把params放到config里面，否在就不把params放在config里面了
+        if (params) config.data = params
+        return request(config)
+    },
+    put(url, params) {
+        const config = {
+            method: 'put',  // 请求方式
+            url: url    // 请求地址
+        }
+        if (params) config.params = params
+        return request(config)
+    },
+    delete(url, params) {
+        const config = {
+            method: 'delete',   // 请求方式
+            url: url    // 请求地址
+        }
+        if (params) config.params = params
+        return request(config)
     }
-
 }
 export default http
